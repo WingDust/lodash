c8ad54777ea0c51d1357b9e66f99b2b23ec6f365
WingDust <45911286+WingDust@users.noreply.github.com>
2022-08-17 20:02:05 +0800

    comment for isPrototype

--- _isPrototype.js
+++ _isPrototype.js
@@ -3,7 +3,7 @@ var objectProto = Object.prototype;
 
 /**
  * Checks if `value` is likely a prototype object.
- *
+ * 检查是否是原型对象 也就是 prototype 
  * @private
  * @param {*} value The value to check.
  * @returns {boolean} Returns `true` if `value` is a prototype, else `false`.
@@ -12,7 +12,16 @@ function isPrototype(value) {
   var Ctor = value && value.constructor,
       proto = (typeof Ctor == 'function' && Ctor.prototype) || objectProto;
 
+  console.log(value,proto);
   return value === proto;
 }
+console.log(
+isPrototype(Object.prototype),
+// isPrototype(Array),
+// isPrototype({}),
+// isPrototype(Object.create(null)),
+// {}.constructor,
+((typeof Object && Object.prototype) || Object.prototype) === Object.prototype
+);
 
 module.exports = isPrototype;
